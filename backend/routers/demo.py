97757ee92e19c12974dcf4e640c50625ecf00f09
Kaminyou <ikaminyou@gmail.com>
2023-07-01 17:34:54 +0800

    update demo data

--- backend/routers/demo.py
+++ backend/routers/demo.py
@@ -5,15 +5,68 @@ router = APIRouter()
 
 @router.get('/patient/info')
 def demo_patient_info() -> dict:
-    return {
-        'name': 'aaa',
-        'age': 'bbb',
+    data = {
+        'msg': 'success',
+        'data': {
+            'name': 'anonymous',
+            'birthday': '1980-03-15',
+            'gender': 'female',
+            'age': 43,
+            'height': 166,
+        },
     }
 
+    return data
 
 @router.get('/patient/gait/data')
-def demo_patient_info() -> dict:
-    return {
-        'stride_length': 100,
-        'stride_width': 200,
+def demo_gait_data() -> dict:
+    data = {
+        'msg': 'success',
+        'data': [
+            {
+                'date': '2021-10-03',
+                'stride_length': 112.4,
+                'stride_width': 23,
+                'stride_time': 1.1,
+                'velocity': 1.4,
+                'cadence': 0.8,
+                'turn_time': 1.2,
+            },
+            {
+                'date': '2021-12-10',
+                'stride_length': 108.5,
+                'stride_width': 21,
+                'stride_time': 1.2,
+                'velocity': 1.5,
+                'cadence': 0.9,
+                'turn_time': 1.1,
+            },
+            {
+                'date': '2022-01-08',
+                'stride_length': 109.7,
+                'stride_width': 23,
+                'stride_time': 1.1,
+                'velocity': 1.4,
+                'cadence': 0.8,
+                'turn_time': 1.2,
+            }
+        ]
     }
+    return data
+
+
+@router.get('/patient/gait/unit')
+def demo_gait_unit() -> dict:
+    data = {
+        'msg': 'success',
+        'data': {
+            'date': 'Date',
+            'stride_length': 'Stride length (cm)',
+            'stride_width': 'Stride width (cm)',
+            'stride_time': 'Stride time (s)',
+            'velocity': 'Velocity (m)',
+            'cadence': 'Cadence (1/s)',
+            'turn_time': 'Turn time (s)',
+        },
+    }
+    return data
