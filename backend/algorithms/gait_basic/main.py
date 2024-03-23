e733a419f3edf3e593d8747b099d8e511d9ae46e
Kaminyou <ikaminyou@gmail.com>
2024-03-23 21:19:56 +0800

    auto fix timestamp txt

--- backend/algorithms/gait_basic/main.py
+++ backend/algorithms/gait_basic/main.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import shutil
 import typing as t
-
+import time
 import docker
 import pandas as pd
 
@@ -34,6 +34,49 @@ def replace_in_filenames(path: str, old_string: str, new_string: str) -> None:
             if old_string in _dir:
                 new_dir = _dir.replace(old_string, new_string)
                 os.rename(os.path.join(root, _dir), os.path.join(root, new_dir))
+
+
+def add_newline_if_missing(file_path: str):
+    # some txt (timeframe) has no \n in the last line and trigger error in depth sensing cpp
+    with open(file_path, 'r+') as file:
+        file_contents = file.read()
+        if file_contents.endswith('\n'):
+            return True
+        else:
+            file.write('\n')
+            return False
+
+
+def fix_timestamp_file(timestamp_file_path: str, json_path: str):
+    indices = []
+    mss = []
+    cnt = 0
+    with open(timestamp_file_path, 'r') as f:
+        for line in f:
+            idx, ms = line.strip().split(',')
+            indices.append(int(idx))
+            mss.append(int(ms))
+            cnt += 1
+    json_cnt = 0
+    for filename in os.listdir(json_path):
+        if not filename.endswith('.json'):
+            continue
+        json_cnt += 1
+
+    if json_cnt < cnt:
+        print('Number of timestamps is more than frames')
+        shutil.copy(timestamp_file_path, timestamp_file_path + '.old')
+        with open(timestamp_file_path, 'w') as f:
+            for i in range(json_cnt):
+                f.write(f'{indices[i]},{mss[i]}\n')
+    elif json_cnt > cnt:
+        print('Number of timestamps is less than frames')
+        shutil.copy(timestamp_file_path, timestamp_file_path + '.old')
+        with open(timestamp_file_path, 'w') as f:
+            for i in range(json_cnt):
+                f.write(f'{i + 1},{round(1000 / 30 * (i + 1))}\n')
+    else:
+        print('Timestamp file is correct')
 
 
 class BasicGaitAnalyzer(Analyzer):
@@ -218,6 +261,9 @@ class SVOGaitAnalyzer(Analyzer):
         output_detectron_mp4_path = os.path.join(data_root_dir, 'out', 'render-detectron.mp4')
         # output_gait_folder = os.path.join(data_root_dir, 'out', f'{file_id}-rgait-output/')
 
+        if not add_newline_if_missing(source_txt_path):
+            print('add a new line to txt')
+
         # convert to avi
         run_container(
             image='zed-env:latest',
@@ -314,6 +360,8 @@ class SVOGaitAnalyzer(Analyzer):
                 docker.types.DeviceRequest(device_ids=['0'], capabilities=[['gpu']]),
             ],
         )
+
+        fix_timestamp_file(timestamp_file_path=source_txt_path, json_path=meta_json_path)
 
         # get xyz
         run_container(
